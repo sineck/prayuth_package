0ce9fa34598cdc6f87351a47015e927e43f9bfdd
Pornchai <65265522+sineck@users.noreply.github.com>
2022-08-06 10:54:01 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup,find_packages
  
 setup(name='prayuth_package',
-      version='0.3',
+      version='0.4',
       description='This is prayuth_package.',
       url='https://github.com/sineck/prayuth_package.git',
       author='Pornchai_A',
       author_email='pornchai.puj@gmail.com',
       license='MIT',
       packages=find_packages(),
-      install_requires= ['numpy==1.21.6','pandas==1.3.5'],
+      install_requires= ['numpy==1.21.6','pandas==1.3.5,<=1.3.5'],
       python_requires='>=3.8',
       zip_safe=False)
