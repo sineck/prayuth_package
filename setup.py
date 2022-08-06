4f0b08c5d8686e830238518e80e242e1045f936a
Pornchai <65265522+sineck@users.noreply.github.com>
2022-08-06 10:29:58 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+ 
+setup(name='prayuth_package',
+      version='0.1',
+      description='This is prayuth_package.',
+      url='https://github.com/sineck/prayuth_package.git',
+      author='pornchai',
+      author_email='pornchai.puj@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires= ['numpy','pandas'],
+      python_requires='>=3.8',
+      zip_safe=False)
